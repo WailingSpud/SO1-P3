adf21d6a7abfe9a86935d9dddaaff2a83c87c385
WailingSpud <sebastian.montoto@rai.usc.es>
2025-10-29 21:18:22 +0100

    Add files via upload

--- Entregable1.c
+++ Entregable1.c
@@ -1,38 +1,38 @@
+//Programa hecho por Sebastián Montoto Peña y Manuel Pérez Cancela
+
 #include <unistd.h> 
 #include <signal.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 
+void alarmaValor(int signal);
 void signalHandler(int signal);
-void signalHandler2(int signal);
 
-volatile double numero = 1;
+double numero = 1;
 
 int main(void){
     
-    signal(SIGALRM,signalHandler);
-    signal(SIGUSR1,signalHandler2);
-    
+    signal(SIGALRM,alarmaValor); //Alarma
+    signal(SIGUSR1,signalHandler);
+    alarm(1); //Primera alarma después de 1 segundo
     
     while( 1 ){
-        
         numero += pow(numero,0.5);
-        alarm(1);
-        printf("Mi sucesión: %lf\n",numero);
-        printf("Cuando desee terminar la sucesión haga kill -USR1 y el pid %d\n",getpid());
-        sleep(1);
-        
+        pause(); //Para hasta recibir una señal que ejecute el signalHandler o termine el programa      
     }
 
     return 0;
 }
 
 
-void signalHandler(int signal){
+void alarmaValor(int signal){
     printf("\n[Señal %d recibida]. Número actual: %lf\n", signal,numero);
+    printf("Cuando desee terminar la sucesión haga kill -USR1 y el pid %d\n",getpid());
+    alarm(1); //Programa la siguiente alarma
 }
 
-void signalHandler2(int signal){
-    printf("El programa se va a terminal con la señal %d\n",signal);
+void signalHandler(int signal){
+    printf("El programa se va a terminar con la señal %d\n",signal);
+    exit (EXIT_SUCCESS);
 }
--- Entregable2.c
+++ Entregable2.c
@@ -1,11 +1,17 @@
+//Programa hecho por Sebastián Montoto Peña y Manuel Pérez Cancela
+
 #define _POSIX_C_SOURCE 200809L
 #include <unistd.h> 
 #include <signal.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/wait.h>
+#include <time.h>
+#include <string.h>
 
 void manejador(int sig);
+char *tiempo_local();
+
 
 int main(void){
 
@@ -28,44 +34,46 @@ int main(void){
         
     pid_t proceso_padre =  getpid();
 
-    printf("Soy el padre y mi pid es %d\n", proceso_padre);
+    printf("[%s] Soy el padre y mi pid es %d\n",tiempo_local() , proceso_padre);
 
     pid_t hijo = fork();
 
     if(hijo == 0){ //Proceso hijo
-        printf("Soy el hijo y voy a crear otro hijo, mi pid es: %d\n", getpid());
+        printf("[%s] Soy el hijo y voy a crear otro hijo, mi pid es: %d\n",tiempo_local() ,getpid());
 
         pid_t nieto = fork();
 
         if(nieto == 0){ //Proceso nieto
-            printf("Soy el proceso nieto y mi pid es: %d\n", getpid());
+            printf("[%s] Soy el proceso nieto y mi pid es: %d\n",tiempo_local() , getpid());
+            sleep(2); //Nieto se duerme para asegurarse de que SIGUSR1 llega primero
             kill(proceso_padre,SIGUSR2); //Envía señal SIGUSR2 al abuelo
-            printf("El proceso nieto envía SIGUSR2 a %d\n", getppid());
-            sleep(5); //Espera antes de enviar la señal para asegurar que el hijo envía SIGUSR1 primero
+            printf("[%s] El proceso nieto envía SIGUSR2 a %d\n",tiempo_local() , proceso_padre);
+            sleep(5); 
             exit(22);
         }
         else{ //Proceso hijo
             kill(proceso_padre, SIGUSR1); //Envía señal SIGUSR1 al abuelo
-            printf("El proceso hijo envía SIGUSR1 a %d\n", getppid());
+            printf("[%s] El proceso hijo envía SIGUSR1 a %d\n",tiempo_local() , getppid());
             pid_t nietoid = wait(&status1);
-            printf("El proceso nieto %d ya terminó con código %d\n",nietoid,WEXITSTATUS(status1));
+            printf("[%s] El proceso nieto %d ya terminó con código %d\n",tiempo_local() ,nietoid,WEXITSTATUS(status1));
             exit(42);
         }
     }
     //Proceso padre
 
-    sleep(7); //Espera para asegurarse de que el nieto envíe su señal también    
+    sleep(9); //Espera para asegurarse de que el nieto envíe su señal también    
     pid_t pidhijo = wait(&status0); //Espera al hijo
 
-    printf("El proceso hijo %d ya terminó con código %d\n",pidhijo,WEXITSTATUS(status0));
+    printf("[%s] El proceso hijo %d ya terminó con código %d\n",tiempo_local() ,pidhijo,WEXITSTATUS(status0));
 
     sigemptyset(&pending); //Inicializa pending
     sigpending(&pending);
     if(sigismember(&pending, SIGUSR1)){
-        printf("La señal SIGUSR1 está pendiente en el proceso padre %d\n", proceso_padre);
+        printf("[%s] La señal SIGUSR1 está pendiente en el proceso padre %d\n",tiempo_local() , proceso_padre);
     }
 
     //Desbloqueo de SIGUSR1
+    printf("[%s] SIGUSR1 va a ser desbloqueado\n",tiempo_local());
     sigprocmask(SIG_UNBLOCK, &mask, NULL);
        
     return 0;
@@ -73,7 +81,16 @@ int main(void){
 
 void manejador(int sig) {
     if (sig == SIGUSR1)
-        printf("[HANDLER]: Proceso %d recibió SIGUSR1\n", getpid());
+        printf("[%s] [HANDLER]: Proceso %d recibió SIGUSR1\n",tiempo_local() , getpid());
     else if (sig == SIGUSR2)
-        printf("[HANDLER]: Proceso %d recibió SIGUSR2\n", getpid());
+        printf("[%s] [HANDLER]: Proceso %d recibió SIGUSR2\n",tiempo_local() , getpid());
+}
+
+char *tiempo_local(){
+    static char hora[9];  // HH:MM:SS + '\0'
+    time_t ahora = time(NULL);
+    struct tm *tlocal = localtime(&ahora);
+
+    strftime(hora, sizeof(hora), "%H:%M:%S", tlocal); //Guarda en hora una string con los campos de hora, minutos y segundos de tlocal (tm)
+    return hora;
 }
